3b8bec4e0549151778a14ac2f31e2d6bd60aeadc
Francois Guibert <fguibert@frozax.com>
2021-04-04 19:51:24 +0200

    working wip

--- __main__.py
+++ __main__.py
@@ -3,21 +3,21 @@ from pathlib import Path
 import requests
 import colorama
 from pprint import pprint
+import zipfile
+from io import BytesIO
 
 # Conf
 #STEAM_PARENT_FOLDER = "D:\\Program Files (x86)"
-RAGNASONG_API_ENDPOINT = "https://ragnasong.com/api/searchMap/?start={start}&difficulty="
+RAGNASONG_API_ENDPOINT = "https://ragnasong.com/api/searchMap/?start={start}&dificulty="
+RAGNASONG_SONG = "https://ragnasong.com/api/map/{id}.zip"
 SONGS_PER_PAGE = 10
 
 def error(msg):
     print(msg)
     sys.exit(1)
 
-#ragnarock_path = Path(STEAM_PARENT_FOLDER) / "Steam" / "steamapps" / "Ragnarock"
-#if not ragnarock_path.exists():
-#    error(f"Can't find Ragnarock folder at {ragnarock_path}")
-#
-custom_songs_path = Path("C:\\Users\Francois\Documents") / "Ragnarock" / "CustomSongs"
+custom_songs_path = Path("C:\\Users\\Francois\\Documents") / "Ragnarock" / "CustomSongs"
+custom_songs_path = Path("D:\\gamedev\\tmp\\ragnarock")
 
 def err(s):
     print(colorama.Fore.RED + s + colorama.Style.RESET_ALL)
@@ -31,21 +31,45 @@ def ok(s):
 def debug(s):
     print(s)
 
-warn("warn")
-err("err")
-ok("ok")
-debug("debug")
-
 if not custom_songs_path.exists():
     warn(f"Create custom songs path {custom_songs_path}")
     custom_songs_path.mkdir(parents=True)
 
 start = 0
+total = 0
+downloaded_songs = []
+for f in custom_songs_path.iterdir():
+    if not f.is_dir():
+        continue
+    try:
+        downloaded_songs.append(int(f.name))
+    except ValueError:
+        continue
+
+ok(f"Currently installed Songs: {len(downloaded_songs)}")
 while True:
     url = RAGNASONG_API_ENDPOINT.format(start=start)
-    pprint(url)
     r = requests.get(url)
-    pprint(r)
-    pprint(r.text)
-    pprint(r.json())
+    d = r.json()
+    if total == 0:
+        total = d["count"]
+        ok(f"Ragnasongs.com Songs count: {total}")
+    for song in d["results"]:
+        song_name = f"{song['artist']} - {song['title']}"
+        song_id = int(song["id"])
+        if song_id in downloaded_songs:
+            ok(f"[ALREADY DOWNLOADED] {song_name}")
+        else:
+            warn(f"[DOWNLOADING...] {song_name}")
+            url_song = RAGNASONG_SONG.format(id=song_id)
+            r_song = requests.get(url_song)
+            with zipfile.ZipFile(BytesIO(r_song.content)) as zf:
+                zf.extractall(custom_songs_path / str(song_id))
+
+        break
+
+    start += SONGS_PER_PAGE
+    if True: #start >= total:
+        ok("=> End")
+        break
     break
